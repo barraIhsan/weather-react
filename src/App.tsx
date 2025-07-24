04e52aacfdf8006b4fdab5d54c1b9f0a5e8b8f73
barraIhsan <57800056+barraIhsan@users.noreply.github.com>
2025-07-24 16:22:57 +0700

    feat: remove geolocation options, dont restrict timeout

--- src/App.tsx
+++ src/App.tsx
@@ -63,7 +63,6 @@ export default function App() {
             setCurrentCity("Enter your city");
           }
         },
-        { enableHighAccuracy: false, timeout: 5000, maximumAge: Infinity },
       );
     }
   }, [weather, forecast]);
